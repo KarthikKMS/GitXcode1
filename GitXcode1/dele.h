e4dc285fe4993b455a4013123599dc78b6297d95
Karthik <karthik.ms@zohocorp.com>
2016-12-05 13:50:16 +0530

    Testing in finder

---
+++ GitXcode1/dele.h
@@ -0,0 +1,13 @@
+//
+//  dele.h
+//  GitXcode1
+//
+//  Created by Karthik M S on 05/12/16.
+//  Copyright © 2016 Karthik M S. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dele : NSObject
+
+@end
---
+++ GitXcode1/dele.m
@@ -0,0 +1,13 @@
+//
+//  dele.m
+//  GitXcode1
+//
+//  Created by Karthik M S on 05/12/16.
+//  Copyright © 2016 Karthik M S. All rights reserved.
+//
+
+#import "dele.h"
+
+@implementation dele
+
+@end
