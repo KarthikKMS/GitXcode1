4bb4dc05808512da4b4a7057999d71c7ce468133
Karthik <karthik.ms@zohocorp.com>
2016-12-05 13:47:57 +0530

    Added Page 3

--- GitXcode1/Page2ViewController.m
+++ GitXcode1/Page2ViewController.m
@@ -23,4 +23,9 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+// Takes you to page 3
+- (IBAction)page3ButtonPressed:(id)sender {
+    [self performSegueWithIdentifier:@"Page2To3Segue" sender:self];
+}
+
 @end
